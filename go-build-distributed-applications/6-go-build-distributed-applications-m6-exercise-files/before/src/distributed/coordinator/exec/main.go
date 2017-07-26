b11a2cd850d92acd0496b63061bc3028176a8118
codeyu <codeyu@gmail.com>
2017-07-26 23:15:30 +0800

    init

---
+++ go-build-distributed-applications/6-go-build-distributed-applications-m6-exercise-files/before/src/distributed/coordinator/exec/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"distributed/coordinator"
+	"fmt"
+)
+
+var dc *coordinator.DatabaseConsumer
+
+func main() {
+	ea := coordinator.NewEventAggregator()
+	dc = coordinator.NewDatabaseConsumer(ea)
+	ql := coordinator.NewQueueListener(ea)
+	go ql.ListenForNewSource()
+
+	var a string
+	fmt.Scanln(&a)
+}
